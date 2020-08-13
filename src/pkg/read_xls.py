df75f3be139c5be4c517e7f7126a8938f657965d
igsnow <zzy716810918@gmail.com>
2020-08-13 15:48:49 +0800

    panadas读取第一个行的标签

--- src/pkg/read_xls.py
+++ src/pkg/read_xls.py
@@ -1,8 +1,12 @@
 import pandas as pd
 
 # 读取数据
-cols_data = pd.read_excel('../../static/Classic low august 2020.xlsx', usecols=[0])
+cols_data = pd.read_excel('../../static/refund.xls', usecols=[0])
 cols_list = cols_data.values.tolist()
+
+# 读取第一行的内容
+labels = list(cols_data.columns.values)
+print(labels)
 
 # 写入文件
 with open('thcode.txt', 'w') as f:
