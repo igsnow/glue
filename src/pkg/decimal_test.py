fba28c38367f50b666909315054d92159d11d6ee
igsnow <zzy716810918@gmail.com>
2020-08-13 15:39:54 +0800

    Decimal 精确计算

---
+++ src/pkg/decimal_test.py
@@ -0,0 +1,9 @@
+from decimal import *
+
+a = 0.52
+b = 0.41
+print(a + b)
+
+aa = Decimal('0.52')
+bb = Decimal('0.41')
+print(aa + bb)
--- src/pkg/read_xls.py
+++ src/pkg/read_xls.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # 读取数据
-cols_data = pd.read_excel('../../static/special_rate .xlsx', usecols=[1])
+cols_data = pd.read_excel('../../static/Classic low august 2020.xlsx', usecols=[0])
 cols_list = cols_data.values.tolist()
 
 # 写入文件
