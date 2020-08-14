b89f1ab6ede94e9ad3d93f30d4ef0c6644f9a679
igsnow <zzy716810918@gmail.com>
2020-08-14 11:21:21 +0800

    excel脚本是否添加头部信息

--- src/pkg/read_xls.py
+++ src/pkg/read_xls.py
@@ -4,9 +4,11 @@ import pandas as pd
 cols_data = pd.read_excel('../../static/refund.xls', usecols=[0])
 cols_list = cols_data.values.tolist()
 
-# 读取第一行的内容
+# 读取第一行的内容，一般是表头，如account
 labels = list(cols_data.columns.values)
-print(labels)
+
+# 是否要包含第一行的内容
+# cols_list.insert(0, cols_data.columns.values)
 
 # 写入文件
 with open('thcode.txt', 'w') as f:
