d791ece81997e81aed6bc9544edc34f02c279c98
igsnow <zzy716810918@gmail.com>
2020-11-13 11:12:55 +0800

    降价提醒通知，但是通知没生效

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 ### 3.贪吃蛇小游戏
 ### 4.nba直播赛事
 ### 5.excel提取指定信息
+### 6.爬取京东价格降价桌面提醒
 
 # 豆瓣源
 -i https://pypi.douban.com/simple/
---
+++ src/game/降价提醒.py
@@ -0,0 +1,33 @@
+"""
+爬取京东商品价格，降价桌面右下角提醒通知
+"""
+import time
+import random
+from bs4 import BeautifulSoup
+from plyer import notification
+from selenium.webdriver import Chrome, ChromeOptions
+
+
+def get_price(url):
+    options = ChromeOptions()
+    options.add_argument("headless")
+    d = Chrome(options=options)
+    d.get(url)
+    soup = BeautifulSoup(d.page_source, "html.parser")
+    d.close()
+    price = float(soup.find("span", class_="price J-p-100016034394").text)
+    return price
+
+
+while True:
+    price = get_price("https://item.jd.com/100016034394.html")
+    print(price)
+    if price <= 7000:
+        print('notice')
+        notification.notify(
+            title="降价提醒",
+            message=f"买！买！买！！！",
+            app_icon="cart.ico",
+            timeout=10)
+        break
+    time.sleep(random.randint(30, 60))
