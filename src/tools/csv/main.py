ea0c569dcad543ad73625f44d7972a17631f668c
igsnow <zzy716810918@gmail.com>
2021-03-24 17:49:43 +0800

    补全其他文档所需字段

--- src/tools/csv/main.py
+++ src/tools/csv/main.py
@@ -16,11 +16,24 @@ Option2Value = []
 Option3Name = []
 Option3Value = []
 Grams = []
+Tracker = []
 Qty = []
+Policy = []
+Service = []
 Price = []
 ComparePrice = []
+Shipping = []
+Taxable = []
+Image = []
+CostPerItem = []
 ImageSrc = []
 ImagePosition = []
+Status = []
+Barcode = []
+Card = []
+SeoTitle = []
+SeoDesc = []
+ImgAltText = []
 
 
 def read_mongoDB():
@@ -50,12 +63,28 @@ def parse_data(data):
 
         _type = item.get('Product').get('Attributes').get('name')
 
-        _tag = item.get('Product').get('categoryNameDesc')
-
         sku = item.get('Product').get('Skus').get('Sku')
         print(len(sku))
         for index, i in enumerate(sku):
             Handle.append(_handle)
+            Tracker.append('来赞宝')
+            Qty.append(i.get('quantity'))
+            Policy.append('continue')
+            Service.append('manual')
+            Price.append(i.get('price'))
+            ComparePrice.append('')
+            Shipping.append('TRUE')
+            Taxable.append('TRUE')
+            Image.append(i.get('Images').get('Image')[0])
+            CostPerItem.append('')
+            Status.append(i.get('Status'))
+            Barcode.append('')
+            Card.append('')
+            SeoTitle.append('')
+            SeoDesc.append('')
+            Tags.append('')
+            ImgAltText.append('')
+
             # 除了Handle字段，其他字段每个商品只出现一次
             if index == 0:
                 Title.append(_title)
@@ -63,14 +92,13 @@ def parse_data(data):
                 Published.append(_published)
                 Vendor.append(_name)
                 Type.append(_type)
-                Tags.append(_tag)
+
             else:
                 Title.append('')
                 Body.append('')
                 Published.append('')
                 Vendor.append('')
                 Type.append('')
-                Tags.append('')
 
             if ('color_family' in i.keys()):
                 Option1Name.append('color')
@@ -79,6 +107,9 @@ def parse_data(data):
             if ('size' in i.keys()):
                 Option2Name.append('size')
                 Option2Value.append(i.get('size'))
+
+            if ('package_weight' in i.keys()):
+                Grams.append(i.get('package_weight') * 1000)
 
 
 def save_excel():
@@ -95,10 +126,23 @@ def save_excel():
     data_df["Option2 Value"] = Option2Value
     # data_df["Option3 Name"] = Option3Name
     # data_df["Option3 Value"] = Option3Value
-    # data_df["Variant Grams"] = Grams
-    # data_df["Variant Inventory Qty"] = Qty
-    # data_df["Variant Price"] = Price
-    # data_df["Variant Compare at Price"] = ComparePrice
+    data_df["Variant Grams"] = Grams
+    data_df["Variant Inventory Tracker"] = Tracker
+    data_df["Variant Inventory Qty"] = Qty
+    data_df["Variant Inventory Policy"] = Policy
+    data_df["Variant Fulfillment Service"] = Service
+    data_df["Variant Price"] = Price
+    data_df["Variant Compare at Price"] = ComparePrice
+    data_df["Variant Requires Shipping"] = Shipping
+    data_df["Variant Taxable"] = Taxable
+    data_df["Variant Image"] = Image
+    data_df["Cost per item"] = CostPerItem
+    data_df["Status"] = Status
+    data_df["Variant Barcode"] = Barcode
+    data_df["Gift Card"] = Card
+    data_df["SEO Title"] = SeoTitle
+    data_df["SEO Description"] = SeoDesc
+    data_df["Image Alt Text"] = ImgAltText
     # data_df["Image Src"] = ImageSrc
     # data_df["Image Position"] = ImagePosition
 
