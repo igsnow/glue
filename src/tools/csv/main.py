de99cdc6b8733d17803a6b104c813c02ddd6a79b
igsnow <zzy716810918@gmail.com>
2021-03-25 16:09:27 +0800

    导出sku表格增加最后一个imgSrc和ImgPosition字段

--- src/tools/csv/main.py
+++ src/tools/csv/main.py
@@ -9,6 +9,8 @@ Vendor = []
 Type = []
 Tags = []
 Published = []
+ImageSrc = []
+ImagePosition = []
 Option1Name = []
 Option1Value = []
 Option2Name = []
@@ -26,8 +28,6 @@ Shipping = []
 Taxable = []
 Image = []
 CostPerItem = []
-ImageSrc = []
-ImagePosition = []
 Status = []
 Barcode = []
 Card = []
@@ -51,22 +51,24 @@ def read_mongoDB():
 
 def parse_data(data):
     for item in data:
-        _handle = item.get('Product').get('ItemId')
+        handle = item.get('Product').get('ItemId')
 
-        _title = item.get('Product').get('Attributes').get('name')
+        title = item.get('Product').get('Attributes').get('name')
 
-        _body = item.get('Product').get('Attributes').get('description')
+        body = item.get('Product').get('Attributes').get('description')
 
-        _published = 'TRUE'
+        published = 'TRUE'
 
-        _name = item.get('Product').get('Attributes').get('brand')
+        name = item.get('Product').get('Attributes').get('name')
 
-        _type = item.get('Product').get('Attributes').get('name')
+        type = item.get('Product').get('Attributes').get('brand')
 
         sku = item.get('Product').get('Skus').get('Sku')
+
         print(len(sku))
+
         for index, i in enumerate(sku):
-            Handle.append(_handle)
+            Handle.append(handle)
             Tracker.append('来赞宝')
             Qty.append(i.get('quantity'))
             Policy.append('continue')
@@ -75,7 +77,10 @@ def parse_data(data):
             ComparePrice.append('')
             Shipping.append('TRUE')
             Taxable.append('TRUE')
-            Image.append(i.get('Images').get('Image')[0])
+            if i.get('Images').get('Image') and len(i.get('Images').get('Image')):
+                Image.append(i.get('Images').get('Image')[0])
+            else:
+                Image.append('')
             CostPerItem.append('')
             Status.append(i.get('Status'))
             Barcode.append('')
@@ -84,32 +89,82 @@ def parse_data(data):
             SeoDesc.append('')
             Tags.append('')
             ImgAltText.append('')
+            Option1Name.append('color')
+            Option2Name.append('size')
 
             # 除了Handle字段，其他字段每个商品只出现一次
             if index == 0:
-                Title.append(_title)
-                Body.append(_body)
-                Published.append(_published)
-                Vendor.append(_name)
-                Type.append(_type)
+                Title.append(title)
+                Body.append(body)
+                Vendor.append(name)
+                Type.append(type)
+                Published.append(published)
+                if i.get('Images').get('Image') and len(i.get('Images').get('Image')):
+                    ImageSrc.append(i.get('Images').get('Image')[0])
+                else:
+                    ImageSrc.append('')
+
+                ImagePosition.append(1)
 
             else:
                 Title.append('')
                 Body.append('')
-                Published.append('')
                 Vendor.append('')
                 Type.append('')
+                Published.append('')
+                ImageSrc.append('')
+                ImagePosition.append('')
 
             if ('color_family' in i.keys()):
-                Option1Name.append('color')
                 Option1Value.append(i.get('color_family'))
+            else:
+                Option1Value.append('')
 
             if ('size' in i.keys()):
-                Option2Name.append('size')
                 Option2Value.append(i.get('size'))
+            else:
+                Option2Value.append('')
 
             if ('package_weight' in i.keys()):
                 Grams.append(i.get('package_weight') * 1000)
+            else:
+                Grams.append('')
+
+        # 将第一个sku的图片放到每一个Handle的最后
+        first_sku = sku and sku[0]
+        first_imgs = first_sku.get('Images').get('Image')
+
+        for index, i in enumerate(first_imgs[1:]):
+            Handle.append(handle)
+            ImageSrc.append(i)
+            ImagePosition.append(index + 2)
+            Title.append('')
+            Body.append('')
+            Vendor.append('')
+            Type.append('')
+            Tags.append('')
+            Published.append('')
+            Option1Name.append('')
+            Option1Value.append('')
+            Option2Name.append('')
+            Option2Value.append('')
+            Grams.append('')
+            Tracker.append('')
+            Qty.append('')
+            Policy.append('')
+            Service.append('')
+            Price.append('')
+            ComparePrice.append('')
+            Shipping.append('')
+            Taxable.append('')
+            Image.append('')
+            CostPerItem.append('')
+            Status.append('')
+            Barcode.append('')
+            Card.append('')
+            SeoTitle.append('')
+            SeoDesc.append('')
+            ImgAltText.append('')
 
 
 def save_excel():
@@ -120,6 +175,8 @@ def save_excel():
     data_df["Type"] = Type
     data_df["Tags"] = Tags
     data_df["Published"] = Published
+    data_df["Image Src"] = ImageSrc
+    data_df["Image Position"] = ImagePosition
     data_df["Option1 Name"] = Option1Name
     data_df["Option1 Value"] = Option1Value
     data_df["Option2 Name"] = Option2Name
@@ -143,11 +200,22 @@ def save_excel():
     data_df["SEO Title"] = SeoTitle
     data_df["SEO Description"] = SeoDesc
     data_df["Image Alt Text"] = ImgAltText
-    # data_df["Image Src"] = ImageSrc
-    # data_df["Image Position"] = ImagePosition
 
     writer = pd.ExcelWriter('sku.xlsx')
     data_df.to_excel(writer, sheet_name='product_template', index=False)
+
+    # workbook = writer.book
+    # worksheet = writer.sheets['product_template']
+
+    # for i, col in enumerate(data_df.columns):
+    #     # find length of column i
+    #     column_len = data_df[col].astype(str).str.len().max()
+    #     # Setting the length if the column header is larger
+    #     # than the max column value length
+    #     column_len = max(column_len, len(col)) + 2
+    #     # set the column length
+    #     worksheet.set_column(i, i, column_len)
+
     writer.save()
 
 
